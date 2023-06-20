4c904cc8df692d8f5efe8f61eda72cd2238862a8
Nosslrac <89852772+Nosslrac@users.noreply.github.com>
2023-06-20 23:04:31 +0200

    Webview of cat

---
+++ src/extension.ts
@@ -0,0 +1,28 @@
+import * as vscode from 'vscode';
+import {getWebviewOptions, GraphView} from './webView'
+
+
+
+
+export function activate(context: vscode.ExtensionContext) {
+	context.subscriptions.push(
+		vscode.commands.registerCommand('graphView.showGraph', () => {
+			GraphView.createOrShow(context.extensionUri);
+		})
+	);
+
+
+	if (vscode.window.registerWebviewPanelSerializer) {
+		// Make sure we register a serializer in activation event
+		vscode.window.registerWebviewPanelSerializer(GraphView.viewType, {
+			async deserializeWebviewPanel(webviewPanel: vscode.WebviewPanel, state: any) {
+				console.log(`Got state: ${state}`);
+				// Reset the webview options so we use latest uri for `localResourceRoots`.
+				webviewPanel.webview.options = getWebviewOptions(context.extensionUri);
+				GraphView.revive(webviewPanel, context.extensionUri);
+			}
+		});
+	}
+}
+
+
---
+++ src/graphDrawer.ts
@@ -0,0 +1,16 @@
+import * as d3 from 'd3'
+d3.select("svg");
+
+
+class GraphDrawer{
+
+	constructor(){
+
+	}
+
+	public startSimulation(){
+
+	}
+
+
+}
